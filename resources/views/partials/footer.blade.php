c62398a970697b9d70ca556dfebe5071d11496e9
Carlo-Falanga <carlofalanga7@gmail.com>
2026-06-16 13:16:20 +0200

    Add footer structure with links and social media icons

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,57 @@
-<footer>
-    Footer
+<footer class="bg-dark text-white pt-5 pb-4">
+    <div class="container">
+        <div class="row">
+            
+            <div class="col-6 col-md-3 mb-4">
+                <h4 class="text-uppercase fw-bold fs-6 mb-3">DC Comics</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-secondary text-decoration-none">Characters</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">Comics</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">Movies</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">TV</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">Games</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">Videos</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">News</a></li>
+                </ul>
+            </div>
+
+            
+            <div class="col-6 col-md-3 mb-4">
+                <h4 class="text-uppercase fw-bold fs-6 mb-3">DC</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-secondary text-decoration-none">Terms Of Use</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">Privacy Policy</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">Advertising</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">Jobs</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">Subscriptions</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">Contact Us</a></li>
+                </ul>
+            </div>
+
+            
+            <div class="col-6 col-md-3 mb-4">
+                <h4 class="text-uppercase fw-bold fs-6 mb-3">Sites</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-secondary text-decoration-none">DC</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">MAD Magazine</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">DC Kids</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">DC Universe</a></li>
+                    <li><a href="#" class="text-secondary text-decoration-none">DC Power Visa</a></li>
+                </ul>
+            </div>
+
+            
+            <div class="col-6 col-md-3 mb-4">
+                <button class="btn btn-outline-primary text-white text-uppercase fw-bold mb-4">Sign-up Now!</button>
+                <h4 class="text-primary text-uppercase fw-bold fs-6 mb-3">Follow Us</h4>
+                <div class="d-flex gap-2">
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="Facebook" width="30"></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="Twitter" width="30"></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="YouTube" width="30"></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="Pinterest" width="30"></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="Periscope" width="30"></a>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
