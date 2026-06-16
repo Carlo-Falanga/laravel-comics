501f7fd82eab4787af30aaaff9cbe54105b9e01e
Carlo-Falanga <carlofalanga7@gmail.com>
2026-06-16 12:14:51 +0200

    Add layout structure and header with logo to home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel - Comics</title>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-</head>
-<body>
-    @include("partials.header")
+@extends("layouts.master")
+
+    @section("contenuto")
     <h1>Hello World</h1>
-    @include("partials.footer")
-</body>
-</html>
+    @endsection
+
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,31 @@
-<header>
-        <nav>
-            <ul>
-                <li>link 1</li>
-            </ul>
-        </nav>
+<header class="bg-white">
+    <nav class="navbar navbar-expand-lg container py-3">
+        <div class="container-fluid">
+            {{-- Logo DC --}}
+            <a class="navbar-brand" href="#">
+                <img src="{{ Vite::asset('resources/img/dc-comics-logo.svg') }}" alt="DC Comics" width="60">
+            </a>
+
+            {{-- Bottone hamburger per mobile --}}
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            {{-- Menu --}}
+            <div class="collapse navbar-collapse justify-content-end" id="mainNav">
+                <ul class="navbar-nav text-uppercase fw-bold">
+                    <li class="nav-item"><a class="nav-link" href="#">Characters</a></li>
+                    <li class="nav-item"><a class="nav-link active text-primary" href="#">Comics</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Movies</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">TV</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Games</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Collectibles</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Videos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Fans</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">News</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Shop</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </header>
